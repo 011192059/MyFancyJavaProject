fc888ef50c5382858e2db1954ddf7e9f18e37732
SiamSarker <msarker192014@bscse.uiu.ac.bd>
2022-05-13 23:47:36 +0600

    branch XY created

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("World");
-
-        System.out.println("This is Branch Y");
+        System.out.println("The second commit");
     }
 }
